2b94fda3aaa7888250cdad6d2cc15c9f1655ce19
ETEC Adolpho Berezin <julioarp2004@gmail.com>
2024-11-29 22:37:44 -0300

    Programa do Silvio Santos: UM, DOIS, TRÊS, PIM

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("-------------------------------------------------");
+
+Console.WriteLine("--- Vai começar o programa UM, DOIS, TRÊS, PIM---");
+
+Console.WriteLine("-------------------------------------------------");
+
+for (int n = 1; n <= 40; n++)
+    Console.Write(n % 4 == 0 ? "pim\n" : $"{n,8} ");
+   
