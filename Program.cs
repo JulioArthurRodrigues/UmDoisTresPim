4d866aa11725038a7ba4e7b562f5f35272a62cf4
ETEC Adolpho Berezin <julioarp2004@gmail.com>
2024-12-03 21:28:25 -0300

    Silvio Santos apresenta: o Programa 1, 2, 3, PIM!!!

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@
 
 Console.WriteLine("-------------------------------------------------");
 
-Console.WriteLine("--- Vai começar o programa UM, DOIS, TRÊS, PIM---");
+Console.WriteLine("------------PROGRAMA DO SILVIO SANTOS------------");
+
+Console.WriteLine("-------------------------------------------------");
+
+Console.WriteLine("--- Vai começar o desafio UM, DOIS, TRÊS, PIM---");
 
 Console.WriteLine("-------------------------------------------------");
 
 for (int n = 1; n <= 40; n++)
     Console.Write(n % 4 == 0 ? "pim\n" : $"{n,8} ");
-   
